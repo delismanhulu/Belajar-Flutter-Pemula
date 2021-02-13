3ca01dca647c4268245cde15f530b7eb2e9f1b9f
delisman <delishulu.com@gmail.com>
2021-02-13 23:15:09 +0700

    first post

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MaterialApp(
+    home: BelajarGetData(),
+  ));
+}
+
+class BelajarGetData extends StatelessWidget {
+  final String apiUrl = "http://store.appdev.my.id/api/products/";
+
+  Future<List<dynamic>> _fecthDataUsers() async {
+    var result = await http.get(apiUrl);
+    return json.decode(result.body)['products'];
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text('API - FLUTTER'),
+      ),
+
+      body: Container(
+        child: FutureBuilder<List<dynamic>>(
+          future: _fecthDataUsers(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  padding: EdgeInsets.all(10),
+                  itemCount: snapshot.data.length,
+
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage:
+                            NetworkImage(snapshot.data[index]['image']),
+                      ),
+
+                      title: Text(snapshot.data[index]['title']),
+                      subtitle: Text(snapshot.data[index]['created_at']),
+                    );
+                  });
+                  
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
